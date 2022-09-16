f7089018f5526a96d55d3151f888bd0e68f0cb7a
esteban <1029estebangt@gmail.com>
2022-09-16 09:37:47 -0500

    funcion multiplicar

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function sumar(){
     let suma=a+b
     console.log(suma)
 }sumar()
+
+let number=+prompt("give me a number")
+function multiplicar(number){
+    for(let i=1; i<=9; i++){
+        document.write(`${number}x${i}=${number*i}</br>`)
+    } 
+}
+multiplicar(number)
